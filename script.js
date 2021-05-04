8fcc5e1f99f39638ccbfde01968bb0025e7c8c45
lilsson <pirckhelavalile@gmail.com>
2021-05-04 16:48:12 +0300

    add js

---
+++ script.js
@@ -0,0 +1,38 @@
+let cars = ["audi","bmw", "lexus", "volkswagen", " por”audi”, sche" ]; 
+
+
+for (let i=0; i< cars.length; i++) {
+    console.log(cars [i])
+};
+
+
+// homework2
+let numbers =[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i=0; i < numbers.length; i++){
+    if (numbers[i] >3){
+        console.log (numbers[i])
+    }
+};
+// 
+
+
+
+
+
+
+
+// hw4
+let cars = ["audi","bmw", "lexus", "volkswagen", " porsche", ["wagen","sche"] ]; 
+
+console.log (cars[6][1]);  
+
+// HW5
+var person = {
+    firstname: "john",
+    lastname: "doe",
+    age: 50,
+    eyecolor: "blue"
+  };
+  Console.log (person.eyecolor);
+  
